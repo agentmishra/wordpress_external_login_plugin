657c5e615b2a4c205602172f703a0c71babf00ce
Tom Benyon <Tom.Benyon@creative-assembly.com>
2017-12-03 07:58:05 +0000

    Add initial plugin file

---
+++ external-login.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: External Login
+Plugin URI: http://tom.benyon.io
+Description: A plugin to allow login and syncing from a secondary database
+Author: Tom Benyon
+Version: 0.1.0
+Author URI: http://tom.benyon.io
+*/
