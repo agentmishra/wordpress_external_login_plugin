bb4b7ebd6bc467a21518c42eb42455339ef1fc3f
Tom Benyon <Tom.Benyon@creative-assembly.com>
2017-12-28 07:39:19 +0000

    Allow user to choose a hashing algorithm

--- hashPassword.php
+++ hashPassword.php
@@ -1,10 +1,14 @@
 <?php
     function exlog_hash_password($password) {
-        if (get_option("external_login_option_db_salt_location") == "before") {
-            return hash('sha512', get_option("external_login_option_db_salt") . $password);
-        } elseif (get_option("external_login_option_db_salt_location") == "after") {
-            return hash('sha512', $password . get_option("external_login_option_db_salt"));
+        $algorithm = get_option("external_login_option_hash_algorithm");
+        $salt = get_option("external_login_option_db_salt");
+        $salt_location = get_option("external_login_option_db_salt_location");
+
+        if ($salt_location == "before") {
+            return hash($algorithm, $salt . $password);
+        } elseif ($salt_location == "after") {
+            return hash($algorithm, $password . $salt);
         } else {
-            return hash('sha512', $password);
+            return hash($algorithm, $password);
         }
     }
