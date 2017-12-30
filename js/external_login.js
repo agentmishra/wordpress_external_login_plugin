a353d9ed66172e8d9389fc475dcc9bec4c494871
Tom Benyon <Tom.Benyon@creative-assembly.com>
2017-12-30 09:11:39 +0000

    Store Role data and retrieve from the server - more work required…
    Still need to work on ensuring the correct name values are given and that the js matches the php

--- js/external_login.js
+++ js/external_login.js
@@ -4,20 +4,6 @@
         var $roles = $(".roles", $role_section);
         var hidden_roles_input_selector = ".exlog_custom_roles";
 
-        $(".add_button", $role_section).click(function () {
-            var $new_item = $(".role", $role_section)
-                .first()
-                .clone()
-                .appendTo($roles);
-
-            $(".external_role", $new_item)
-                .attr("readonly", false)
-                .attr("value", "");
-            $(".description", $new_item).remove();
-
-            watchRoleTextInputs();
-        });
-
         function watchRoleTextInputs() {
             $(".external_role", $role_section)
                 .change(function () {
@@ -36,7 +22,7 @@
                     var external_role_text = $(".external_role", $(this)).attr("value");
                     var wordpress_role = $(".wordpress_role", $(this)).attr("value");
                     roles_data.push({
-                        "external_role_text": external_role_text,
+                        "external_role": external_role_text,
                         "wordpress_role": wordpress_role
                     })
                 });
@@ -45,5 +31,21 @@
                 $(hidden_roles_input_selector, $role_section).attr("value", input_ready_roles);
             }
         }
+
+        $(".add_button", $role_section).click(function () {
+            var $new_item = $(".role", $role_section)
+                .first()
+                .clone()
+                .appendTo($roles);
+
+            $(".external_role", $new_item)
+                .attr("readonly", false)
+                .attr("value", "");
+            $(".description", $new_item).remove();
+
+            watchRoleTextInputs();
+        });
+
+        watchRoleTextInputs();
     })
 }(jQuery));
--- views/form_elements/roles_fields_builder.php
+++ views/form_elements/roles_fields_builder.php
@@ -1,14 +1,22 @@
 <?php
+  $EXLOG_UNSPECIFIED_ROLE_STRING = "UNSPECIFIED";
+
   $exlog_roles = array_reverse(exlog_get_roles());
 
-  var_dump(get_option($form_field["field_slug"]));
+  $exlog_external_roles = get_option($form_field["field_slug"]);
 
-  $exlog_external_roles = array(
-    array(
-      "external_db_role" => "UNSPECIFIED",
-      "wordpress_role" => "",
-    )
-  );
+  if ($exlog_external_roles) {
+      $exlog_external_roles = json_decode(urldecode($exlog_external_roles), true);
+  } else {
+      $exlog_external_roles = array(
+          array(
+              "external_role" => $EXLOG_UNSPECIFIED_ROLE_STRING,
+              "wordpress_role" => "",
+          )
+      );
+  };
+
+
 ?>
 
 
@@ -19,8 +27,10 @@
       <input
         class="external_role"
         type="text"
-        value="<?php echo $exlog_external_role['external_db_role']; ?>"
-        readonly
+        value="<?php echo $exlog_external_role['external_role']; ?>"
+        <?php if ($exlog_external_role['external_role'] == $EXLOG_UNSPECIFIED_ROLE_STRING) : ?>
+          readonly
+        <?php endif; ?>
       >
 
       <select class="wordpress_role" name="<?php echo "REFFFFFFFFFFFFFFFFFFF"; ?>">
