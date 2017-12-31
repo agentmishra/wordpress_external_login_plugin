dc8be32acf2c0ed2f895e021cdcdb9c103cdc5ab
Tom Benyon <Tom.Benyon@creative-assembly.com>
2017-12-31 09:08:03 +0000

    Add delete button for key pair values

--- js/external_login.js
+++ js/external_login.js
@@ -47,6 +47,11 @@
                 $(this).attr("name", slugged_wordpress_role);
                 update_roles_data();
             });
+
+            $(".remove_role_pairing", $role_section).click(function () {
+                $(this).parent().remove();
+                update_roles_data();
+            });
         }
 
         $(".add_button", $role_section).click(function () {
@@ -60,6 +65,13 @@
                 .attr("value", "");
             $(".description", $new_item).remove();
             $(".wordpress_role", $new_item)[0].selectedIndex = 0;
+
+            var button = $(document.createElement('input'))
+                .attr("type", "button")
+                .attr("class", "remove_role_pairing")
+                .attr("value", "Delete");
+
+            $new_item.append(button);
 
             watchRoleTextInputs();
         });
