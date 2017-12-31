48760232063dbd3ee7d21c627d92bdff4ebaff9b
Tom Benyon <Tom.Benyon@creative-assembly.com>
2017-12-31 08:09:02 +0000

    Ensure the correct name values are stored against role items

--- js/external_login.js
+++ js/external_login.js
@@ -4,32 +4,42 @@
         var $roles = $(".roles", $role_section);
         var hidden_roles_input_selector = ".exlog_custom_roles";
 
+        var external_role_name_prefix = $roles.attr("data-exlog-external-role-prefix");
+        var wordpress_role_name_prefix = $roles.attr("data-exlog-wordpress-role-prefix");
+
+        function update_roles_data() {
+            var roles_data = [];
+            $(".role", $role_section).each(function () {
+                var $this = $(this);
+                roles_data.push({
+                    "external_role_value": $(".external_role", $this).attr("value"),
+                    "wordpress_role_value": $(".wordpress_role", $this).attr("value"),
+                    "wordpress_role_name": $(".wordpress_role", $this).attr("name"),
+                    "external_role_name": $(".external_role", $this).attr("name")
+                })
+            });
+
+            var input_ready_roles = window.exlog.json_to_input(roles_data);
+            $(hidden_roles_input_selector, $role_section).attr("value", input_ready_roles);
+        }
+
         function watchRoleTextInputs() {
             $(".external_role", $role_section)
                 .change(function () {
                     var value = $(this).attr("value");
                     var slugged_value = window.exlog.slugify($(this).attr("value"));
-                    $(this).attr("name", "exlog_role_" + slugged_value);
+                    var slugged_external_role = external_role_name_prefix + slugged_value;
+                    $(this).attr("name", slugged_external_role);
                     update_roles_data();
                 });
 
-            $(".wordpress_role", $role_section).change(update_roles_data);
-
-
-            function update_roles_data() {
-                var roles_data = [];
-                $(".role", $role_section).each(function () {
-                    var external_role_text = $(".external_role", $(this)).attr("value");
-                    var wordpress_role = $(".wordpress_role", $(this)).attr("value");
-                    roles_data.push({
-                        "external_role": external_role_text,
-                        "wordpress_role": wordpress_role
-                    })
-                });
-
-                var input_ready_roles = window.exlog.json_to_input(roles_data);
-                $(hidden_roles_input_selector, $role_section).attr("value", input_ready_roles);
-            }
+            $(".wordpress_role", $role_section).change(function () {
+                var external_role_value = $(this).parent().find(".external_role").attr("value");
+                var slugged_value = window.exlog.slugify(external_role_value);
+                var slugged_wordpress_role = wordpress_role_name_prefix + slugged_value;
+                $(this).attr("name", slugged_wordpress_role);
+                update_roles_data();
+            });
         }
 
         $(".add_button", $role_section).click(function () {
@@ -46,6 +56,7 @@
             watchRoleTextInputs();
         });
 
+        update_roles_data();
         watchRoleTextInputs();
     })
 }(jQuery));
--- views/form_elements/roles_fields_builder.php
+++ views/form_elements/roles_fields_builder.php
@@ -1,7 +1,15 @@
 <?php
-  $EXLOG_UNSPECIFIED_ROLE_STRING = "UNSPECIFIED";
-
   $exlog_roles = array_reverse(exlog_get_roles());
+  reset($exlog_roles);
+  $exlog_first_role = key($exlog_roles);
+
+  $EXLOG_EXTERNAL_ROLE_PREFIX = "exlog_role_external_";
+  $EXLOG_WORDPRESS_ROLE_PREFIX = "exlog_role_wordpress_";
+
+  $EXLOG_UNSPECIFIED_EXTERNAL_ROLE_VALUE = "UNSPECIFIED";
+  $EXLOG_UNSPECIFIED_EXTERNAL_ROLE_NAME = $EXLOG_EXTERNAL_ROLE_PREFIX . strtolower($EXLOG_UNSPECIFIED_EXTERNAL_ROLE_VALUE);
+  $EXLOG_UNSPECIFIED_WORDPRESS_ROLE_NAME = $EXLOG_WORDPRESS_ROLE_PREFIX . strtolower($EXLOG_UNSPECIFIED_EXTERNAL_ROLE_VALUE);
+  $EXLOG_UNSPECIFIED_WORDPRESS_ROLE_VALUE = $exlog_first_role;
 
   $exlog_external_roles = get_option($form_field["field_slug"]);
 
@@ -10,33 +18,40 @@
   } else {
       $exlog_external_roles = array(
           array(
-              "external_role" => $EXLOG_UNSPECIFIED_ROLE_STRING,
-              "wordpress_role" => "",
+              "external_role_value" => $EXLOG_UNSPECIFIED_EXTERNAL_ROLE_VALUE,
+              "external_role_name" => $EXLOG_UNSPECIFIED_EXTERNAL_ROLE_NAME,
+              "wordpress_role_value" => $EXLOG_UNSPECIFIED_WORDPRESS_ROLE_VALUE,
+              "wordpress_role_name" => $EXLOG_UNSPECIFIED_WORDPRESS_ROLE_NAME,
           )
       );
   };
 
-
 ?>
 
-
-
-<div class="roles">
+<div
+  class="roles"
+  data-exlog-external-role-prefix="<?php echo $EXLOG_EXTERNAL_ROLE_PREFIX; ?>"
+  data-exlog-wordpress-role-prefix="<?php echo $EXLOG_WORDPRESS_ROLE_PREFIX; ?>"
+>
   <?php foreach ($exlog_external_roles as $exlog_external_role) : ?>
     <div class="role">
       <input
         class="external_role"
         type="text"
-        value="<?php echo $exlog_external_role['external_role']; ?>"
-        <?php if ($exlog_external_role['external_role'] == $EXLOG_UNSPECIFIED_ROLE_STRING) : ?>
+        value="<?php echo $exlog_external_role['external_role_value']; ?>"
+        name="<?php echo $exlog_external_role['external_role_name']; ?>"
+        <?php if ($exlog_external_role['external_role_value'] == $EXLOG_UNSPECIFIED_EXTERNAL_ROLE_VALUE) : ?>
           readonly
         <?php endif; ?>
       >
 
-      <select class="wordpress_role" name="<?php echo "REFFFFFFFFFFFFFFFFFFF"; ?>">
+      <select class="wordpress_role" name="<?php echo $exlog_external_role['wordpress_role_name']; ?>">
           <?php foreach ($exlog_roles as $key => $value) : ?>
               <option
-                  <?php if ("SOMETHING" == $key) :?>
+                <?php error_log("\n\n"); ?>
+                <?php error_log($exlog_external_role['wordpress_role_value']); ?>
+                <?php error_log($value); ?>
+                  <?php if ($exlog_external_role['wordpress_role_value'] == $key) :?>
                       selected="selected"
                   <?php endif; ?>
                   value="<?php echo $key; ?>"><?php echo $value; ?>
