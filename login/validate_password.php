2089e72c9e4182b016d30cfa8b2c8ef34aa15433
Tom Benyon <Tom.Benyon@creative-assembly.com>
2018-01-27 20:47:44 +0000

    Allow login systems with no salts to work

--- login/validate_password.php
+++ login/validate_password.php
@@ -1,10 +1,12 @@
 <?php
-    function exlog_hash_password($password) {
+    function exlog_hash_password($password, $no_salt = false) {
         $algorithm = get_option("external_login_option_hash_algorithm");
         $salt = get_option("external_login_option_db_salt");
         $salt_location = get_option("external_login_option_db_salt_location");
 
-        if ($salt_location == "before") {
+        if ($no_salt) {
+            return hash($algorithm, $password);
+        } else if ($salt_location == "before") {
             return hash($algorithm, $salt . $password);
         } elseif ($salt_location == "after") {
             return hash($algorithm, $password . $salt);
@@ -14,10 +16,16 @@
     }
 
     function exlog_validate_password($password, $hash) {
+        $salt_method = get_option("external_login_option_db_salting_method");
         $algorithm = get_option("external_login_option_hash_algorithm");
+
         if ($algorithm == "bcrypt") {
             return password_verify($password, $hash);
         } else {
-            return exlog_hash_password($password) == $hash;
+            if ($salt_method == 'none') {
+                return exlog_hash_password($password, true) == $hash;
+            } else {
+                return exlog_hash_password($password) == $hash;
+            }
         }
     }
