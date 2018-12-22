6be65e5377ad21c5fd71fd3e048e04baf3f6276d
Tom Benyon <Tom.Benyon@creative-assembly.com>
2018-12-22 14:44:41 +0000

    Remove unnecessary logging

--- external-login.php
+++ external-login.php
@@ -18,7 +18,6 @@ define( 'EXLOG_PATH_PLUGIN_TOOLS', EXLOG_PATH_PLUGIN_BASE . '/tools');
 define( 'EXLOG_PATH_PLUGIN_LIB', EXLOG_PATH_PLUGIN_BASE . '/lib');
 define( 'EXLOG_PATH_PLUGIN_SANITISATION_VALIDATION', EXLOG_PATH_PLUGIN_BASE . '/sanitisation_validation');
 define( 'EXLOG_PATH_ASSETS', plugins_url( '/assets/in_app', EXLOG_PLUGIN_FILE_PATH ));
-error_log(EXLOG_PATH_ASSETS);
 
 include EXLOG_PATH_PLUGIN_OPTIONS . '/wpconfig_options.php';
 include EXLOG_PATH_PLUGIN_SANITISATION_VALIDATION . '/validation.php';
