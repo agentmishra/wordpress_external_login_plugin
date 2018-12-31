fae982f6adb8f84ca6e0b1bf2cc16cef457703dd
Tom Benyon <Tom.Benyon@creative-assembly.com>
2018-12-31 16:06:21 +0000

    Fix misplaced bracket causing error with sizeof function

--- views/form_elements/roles_fields_builder.php
+++ views/form_elements/roles_fields_builder.php
@@ -34,7 +34,7 @@
   >
     <h4><?php echo $form_field["field_name"]; ?></h4>
     <p><?php echo $form_field["field_description"]; ?></p>
-    <?php if (is_array($exlog_external_roles) && sizeof($exlog_external_roles > 0)) : ?>
+    <?php if (is_array($exlog_external_roles) && sizeof($exlog_external_roles) > 0) : ?>
       <?php foreach ($exlog_external_roles as $exlog_external_role) : ?>
             <?php include EXLOG_PATH_PLUGIN_VIEWS . '/form_elements/role_field.php'; ?>
         <?php endforeach; ?>
