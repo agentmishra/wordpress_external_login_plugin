1d75650463af0de2d94c20a53602088808a8e5bf
Tom Benyon <Tom.Benyon@creative-assembly.com>
2019-01-29 18:55:58 +0000

    Add example additional field with validation

--- registration/Exlog_user_registration.php
+++ registration/Exlog_user_registration.php
@@ -10,6 +10,7 @@ class Exlog_user_registration {
 
             //Fired when user tries to registers a user. See "Source" here: https://developer.wordpress.org/reference/functions/register_new_user/
             add_filter('register_post', array($self, "before_user_is_registered"), 10, 3 );
+            add_filter('register_form', array($self, "add_additional_fields"));
         }
     }
 
@@ -22,9 +23,26 @@ class Exlog_user_registration {
 
             } else if (exlog_does_value_exists_in_field(exlog_get_option('exlog_dbstructure_email'), $user_email)) {
                 $errors->add('email_exists', "The e-mail is already registered to another user.");
+            } else if (empty( $_POST['first_name'] ) ) {
+                $errors->add( 'first_name_error', __( '<strong>ERROR</strong>: You must add your First Name.', 'crf' ) );
             }
         }
 
         return $errors;
+    }
+
+    public function add_additional_fields() {
+        ?>
+        <p>
+            <label for="first_name"><?php echo "First Name"; ?><br/>
+                <input type="text"
+                       name="first_name"
+                       value=""
+                       class="input"
+                       required
+                />
+            </label>
+        </p>
+        <?php
     }
 }
