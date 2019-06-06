b2eee2fdccd3f25d0a109205db73f2d4621702be
Tom Benyon <Tom.Benyon@creative-assembly.com>
2019-06-06 21:15:32 +0100

    Add migration mode support to the authentication flow

--- login/authenticate.php
+++ login/authenticate.php
@@ -1,79 +1,85 @@
 <?php
 function exlog_auth( $user, $username, $password ){
-    // Make sure a username and password are present for us to work with
-    if($username == '' || $password == '') return;
+    $migration_mode = exlog_get_option('external_login_option_migration_mode') == "on";
 
-    $response = exlog_auth_query($username, $password);
+    // If not in migration mode or you are in migration mode and the username isn't in the WordPress DB
+    if (!$migration_mode || ($migration_mode && !username_exists($username))) {
 
-    $roles = exlog_map_role($response['role']);
+        // Make sure a username and password are present for us to work with
+        if ($username == '' || $password == '') return;
 
-    $block_access_due_to_role = true;
-    foreach ($roles as $role) {
-        if ($role != EXLOG_ROLE_BLOCK_VALUE) {
-            $block_access_due_to_role = false;
-        }
-    }
+        $response = exlog_auth_query($username, $password);
 
-    // If a user was found
-    if ($response) {
+        $roles = exlog_map_role($response['role']);
 
-        // If role is blocking user access
-        if( $block_access_due_to_role ) {
-            $user = new WP_Error( 'denied', __("You are not allowed access") );
-
-        // If user was NOT authenticated
-        } else if( !($response["exlog_authenticated"]) ) {
-            // User does not exist, send back an error message
-            $user = new WP_Error( 'denied', __("Invalid username or password") );
-
-        // If user was authenticated
-        } else if( $response["exlog_authenticated"] ) {
-            // External user exists, try to load the user info from the WordPress user table
-            $userobj = new WP_User();
-            $user = $userobj->get_data_by( 'login', $response['username'] ); // Does not return a WP_User object 🙁
-            $user = new WP_User($user->ID); // Attempt to load up the user with that ID
-
-            $userdata = array(
-                'user_login' => $response['username'],
-                'first_name' => $response['first_name'],
-                'last_name'  => $response['last_name'],
-                'user_pass'  => $password,
-                'role'       => $roles[0],
-                'user_email' => $response['email'],
-            );
-
-            // If user does not exist
-            if( $user->ID == 0 ) {
-                // Setup the minimum required user information
-
-                $new_user_id = wp_insert_user( $userdata ); // A new user has been created
-
-                // Load the new user info
-                $user = new WP_User ($new_user_id);
-            } else {
-                $userdata['ID'] = $user->ID;
-
-                add_filter( 'send_password_change_email', '__return_false' ); // Prevent password update e-mail
-
-                wp_update_user( $userdata );
-            }
-
-            $user->set_role($roles[0]); // Wipe out old roles
-
-            // Add roles to user if more than one
-            foreach ($roles as $role) {
-                $user->add_role($role);
+        $block_access_due_to_role = true;
+        foreach ($roles as $role) {
+            if ($role != EXLOG_ROLE_BLOCK_VALUE) {
+                $block_access_due_to_role = false;
             }
         }
-    }
 
-    // Whether to disable login fallback with the local Wordpress version of the username and password
-    // Prevents local login if:
-    // - Disable local login  is set in the admin area
-    // - OR
-    // - The user was found but the password was rejected
-    if (exlog_get_option('external_login_option_disable_local_login') == "on" || is_wp_error($user)) {
-        remove_action('authenticate', 'wp_authenticate_username_password', 20);
+        // If a user was found
+        if ($response) {
+
+            // If role is blocking user access
+            if ($block_access_due_to_role) {
+                $user = new WP_Error('denied', __("You are not allowed access"));
+
+                // If user was NOT authenticated
+            } else if (!($response["exlog_authenticated"])) {
+                // User does not exist, send back an error message
+                $user = new WP_Error('denied', __("Invalid username or password"));
+
+                // If user was authenticated
+            } else if ($response["exlog_authenticated"]) {
+                // External user exists, try to load the user info from the WordPress user table
+                $userobj = new WP_User();
+                $user = $userobj->get_data_by('login', $response['username']); // Does not return a WP_User object 🙁
+                $user = new WP_User($user->ID); // Attempt to load up the user with that ID
+
+                $userdata = array(
+                    'user_login' => $response['username'],
+                    'first_name' => $response['first_name'],
+                    'last_name' => $response['last_name'],
+                    'user_pass' => $password,
+                    'role' => $roles[0],
+                    'user_email' => $response['email'],
+                );
+
+                // If user does not exist
+                if ($user->ID == 0) {
+                    // Setup the minimum required user information
+
+                    $new_user_id = wp_insert_user($userdata); // A new user has been created
+
+                    // Load the new user info
+                    $user = new WP_User ($new_user_id);
+                } else {
+                    $userdata['ID'] = $user->ID;
+
+                    add_filter('send_password_change_email', '__return_false'); // Prevent password update e-mail
+
+                    wp_update_user($userdata);
+                }
+
+                $user->set_role($roles[0]); // Wipe out old roles
+
+                // Add roles to user if more than one
+                foreach ($roles as $role) {
+                    $user->add_role($role);
+                }
+            }
+        }
+
+        // Whether to disable login fallback with the local Wordpress version of the username and password
+        // Prevents local login if:
+        // - Disable local login  is set in the admin area
+        // - OR
+        // - The user was found but the password was rejected
+        if (exlog_get_option('external_login_option_disable_local_login') == "on" || is_wp_error($user)) {
+            remove_action('authenticate', 'wp_authenticate_username_password', 20);
+        }
     }
 
     return $user;
